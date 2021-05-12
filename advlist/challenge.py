e2ede9b846abcaf5d6a65733db2ee825c83be6a3
Wenhao Piao <wenhaopiao520@gmail.com>
2021-05-12 16:01:21 +0000

    Add new files and delete swap files

---
+++ advlist/challenge.py
@@ -0,0 +1,4 @@
+icecream = ["flavors", "salty"]
+icecream.append(99)
+name = input("What is your name\n> ")
+print(f"{icecream[2]} {icecream[0]}, and {name} choose to be {icecream[1]}.")
---
+++ print.py
@@ -0,0 +1,6 @@
+year = input("What year is it?")
+name = input("What is your name?")
+
+print("My name is {}, it is the year {}".format(name, year))
+print("hello world")
+
