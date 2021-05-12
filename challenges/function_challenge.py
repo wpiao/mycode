e3371f063d369f566949f0e83169e4b24ba736ed
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-05-12 16:25:18 -0700

    Resolve the path issue so that this file can be run from any directory

--- challenges/function_challenge.py
+++ challenges/function_challenge.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import os
 # CREATE A FUNCTION that finds the three entries that sum to 2020 and multiply them together.
-# How to use this function
-# Run "wget https://static.alta3.com/courses/pyb/puzzle1.txt" to download the file in the same folder 
+# How to use this function:
+# Run "wget https://static.alta3.com/courses/pyb/puzzle1.txt" to download the file in the same folder with this py file
 def find_three_numbers(file_name):
-  cwd = os.getcwd()
-  with open(f"{cwd}/{file_name}") as file:
+  dirname = os.path.dirname(os.path.realpath(__file__))
+  file_path = os.path.join(dirname, file_name)
+  with open(file_path) as file:
     numbers = [line.rstrip() for line in file]
     for i in range(len(numbers) - 2):
       for j in range(i + 1, len(numbers) - 1):
