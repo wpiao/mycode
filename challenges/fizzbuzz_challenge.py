b2e6555d21aedb0f5d7783c06fe0f72f379fb507
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-05-14 07:48:46 -0700

    Complete fizzbuzz

---
+++ challenges/fizzbuzz_challenge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def fizzbuzz(max):
+  for i in range(1, max + 1):
+    if (i % 3 == 0 and i % 5 == 0):
+      print("FizzBuzz")
+    elif (i % 3 == 0):
+      print("Fizz")
+    elif (i % 5 == 0):
+      print("Buzz")
+    else:
+      print(i)
+
+fizzbuzz(100)
